9800425a8286d0ef171c646594e09737ee14132e
zhangjiayu <790240736@qq.com>
2018-04-13 09:18:19 +0800

    解决了SQLSession没有自动注入的问题 原因  @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})   之前一直排除了数据库的自动配置

--- nsfc-platform-ws/src/main/java/com/nsfc/platform/base/PlatformWsApplication.java
+++ nsfc-platform-ws/src/main/java/com/nsfc/platform/base/PlatformWsApplication.java
@@ -1,13 +1,15 @@
 package com.nsfc.platform.base;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //排除数据库自动配置影响
+@SpringBootApplication //排除数据库自动配置影响
 @ComponentScan("com.nsfc")
-public class PlatformWsApplication {
+@MapperScan("com.nsfc.platform.*.mapper")//将项目中对应的mapper类的路径加进来就可以了
+public class PlatformWsApplication { //加上这个是为了使用外部分容器
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlatformWsApplication.class, args);
