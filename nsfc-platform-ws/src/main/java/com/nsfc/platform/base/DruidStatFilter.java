652063084c08eebf353c668ec683de68cc585788
zhangjiayu <790240736@qq.com>
2018-05-04 18:15:03 +0800

    添加Druid连接池配置和监控  感觉很强大

---
+++ nsfc-platform-ws/src/main/java/com/nsfc/platform/base/DruidStatFilter.java
@@ -0,0 +1,20 @@
+package com.nsfc.platform.base;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * druid过滤器.
+ * @author zhangjiayu zhangjiayu
+ * @create 2018/5/4
+ */
+@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
+        initParams={
+                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")//忽略资源
+        }
+)
+public class DruidStatFilter extends WebStatFilter {
+
+}
